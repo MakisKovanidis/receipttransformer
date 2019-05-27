d30abc857b35e4847207ab9faf5922e8a67da301
Makis <makis_kovanidis@hotmail.com>
2019-05-27 12:07:33 +0300

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws Exception
+    {
+        // pass the path to the file as a parameter
+        File file = new File("C:\\test\\Receipt.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        while ((st = br.readLine().) != null)
+            String twt = new String( st , )
+            System.out.println(st);
+    }
+}
